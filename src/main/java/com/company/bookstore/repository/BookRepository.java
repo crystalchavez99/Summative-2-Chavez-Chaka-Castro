23693f609f58d0a87e3cfd3435dea41f613088a7
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-08-06 17:50:39 -0700

    book repo

--- src/main/java/com/company/bookstore/repository/BookRepository.java
+++ src/main/java/com/company/bookstore/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.company.bookstore.repository;
 
 import com.company.bookstore.models.Book;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    List<Book> findBookByAuthorId(int authorId);
+
+    List<Book> findByAuthorId(int authorId);
+
 }
