3c90cb12c8152f339eb76f22e440682662edfa7c
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-08-06 17:52:47 -0700

    graph controller

--- src/main/java/com/company/bookstore/controller/GraphController.java
+++ src/main/java/com/company/bookstore/controller/GraphController.java
@@ -2,35 +2,64 @@ package com.company.bookstore.controller;
 
 import com.company.bookstore.models.Book;
 import com.company.bookstore.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Controller;
+
 
 import com.company.bookstore.models.Publisher;
 import com.company.bookstore.repository.PublisherRepository;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Optional;
 
-
 @Controller
 public class GraphController {
-
-
+    @Autowired
+    PublisherRepository publisherRepository;
 
     @Autowired
     BookRepository bookRepository;
 
     @Autowired
-    PublisherRepository publisherRepository;
-
+    AuthorRepository authorRepository;
 
     @QueryMapping
-    public List<Book> books(){
+    public List<Publisher> publishers() {
+        return publisherRepository.findAll();
+    }
+
+    @QueryMapping
+    public List<Book> books() {
         return bookRepository.findAll();
+    }
+
+    @QueryMapping
+    public List<Author> authors() {
+        return authorRepository.findAll();
+    }
+
+    @QueryMapping
+    public Publisher findPublisherById(@Argument int id) {
+        Optional<Publisher> returnVal = publisherRepository.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
+    }
+
+    @QueryMapping
+    public Author findAuthorById(@Argument int id) {
+        Optional<Author> returnVal = authorRepository.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
     }
 
     @QueryMapping
@@ -43,105 +72,24 @@ public class GraphController {
         }
     }
 
-//    @QueryMapping
-//    public Book findBookByAuthorId(@Argument String authorId){
-//        return bookRepository.getBookByAuthorId(authorId);
-//    }
-//
-//    @MutationMapping
-//    public Book addBook(
-//            @Argument String id,
-//            @Argument String isbn,
-//            @Argument String publishDate,
-//            @Argument int authorId,
-//            @Argument String title,
-//            @Argument int publisherId,
-//            @Argument float price
-//            ){
-//        return bookRepository.addBook(id,isbn,publishDate,authorId, title,publisherId,price);
-//    }
-
-//    @MutationMapping
-//    public Book updateBook(
-//            @Argument String id,
-//            @Argument String isbn,
-//            @Argument String publishDate,
-//            @Argument int authorId,
-//            @Argument String title,
-//            @Argument int publisherId,
-//            @Argument float price
-//    ){
-//        Book updateBook = new Book(id,isbn,publishDate,authorId, title,publisherId,price);
-//        return bookRepository.updateBook(updateBook);
-//    }
-//
-//    @MutationMapping
-//    public boolean deleteBookById(@Argument String id){
-//        return bookRepository.deleteBookById(id);
-//    }
-
-
-//PublisherController
-
-    @QueryMapping
-    public List<Publisher> publishers() {
-        return publisherRepository.getAllPublishers();
+    @SchemaMapping
+    public Publisher publisher(Book book) {
+        Optional<Publisher> returnVal = publisherRepository.findById(book.getPublisherId());
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
     }
 
-    @QueryMapping
-    public Publisher findPublisherById(@Argument String id) {
-        return publisherRepository.getPublisherById(id);
+    @SchemaMapping
+    public Author author(Book book) {
+        Optional<Author> returnVal = authorRepository.findById(book.getAuthorId());
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
     }
-
-    @MutationMapping
-    public Publisher addPublisher(
-            @Argument String name,
-            @Argument String street,
-            @Argument String city,
-            @Argument String state,
-            @Argument String postalCode,
-            @Argument String phone,
-            @Argument String email
-    ) {
-        Publisher newPublisher = new Publisher();
-        newPublisher.setName(name);
-        newPublisher.setStreet(street);
-        newPublisher.setCity(city);
-        newPublisher.setState(state);
-        newPublisher.setPostalCode(postalCode);
-        newPublisher.setPhone(phone);
-        newPublisher.setEmail(email);
-        return publisherRepository.addPublisher(newPublisher);
-    }
-
-    @MutationMapping
-    public Publisher updatePublisher(
-            @Argument String name,
-            @Argument String street,
-            @Argument String city,
-            @Argument String state,
-            @Argument String postalCode,
-            @Argument String phone,
-            @Argument String email
-    ) {
-        Publisher updatePublisher = new Publisher();
-        updatePublisher.setName(name);
-        updatePublisher.setStreet(street);
-        updatePublisher.setCity(city);
-        updatePublisher.setState(state);
-        updatePublisher.setPostalCode(postalCode);
-        updatePublisher.setPhone(phone);
-        updatePublisher.setEmail(email);
-        return publisherRepository.updatePublisher(updatePublisher);
-    }
-
-    @MutationMapping
-    public boolean deletePublisherById(@Argument String id) {
-        return publisherRepository.deletePublisherById(id);
-    }
-
 
 }
-
-
-
